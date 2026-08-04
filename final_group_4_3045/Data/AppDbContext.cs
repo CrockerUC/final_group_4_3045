a6ff1e15cd40c1342da2ae1e01fb512d7cad803d
CrockerUC <crockecf@mail.uc.edu>
2026-08-04 18:57:36 -0400

    Added Data folder and AppDbContext.cs file to bridge between C# code and SQL database

---
+++ final_group_4_3045/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using final_group_4_3045.Models;
+
+namespace final_group_4_3045.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        // Define DbSet properties for each model
+        public DbSet<TeamMember> TeamMembers { get; set; }
+        public DbSet<Hobby> Hobbies { get; set; }
+        public DbSet<BreakfastFood> BreakfastFoods { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
